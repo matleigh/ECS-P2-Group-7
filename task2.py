4a521b875062357ecb4a42a71f7cdf982b199c5a
Tamars252 <113263334+Tamars252@users.noreply.github.com>
2024-10-29 09:48:37 -0400

    Changed to python file

--- task2.py
+++ task2.py
@@ -1,12 +1,11 @@
 #Tamar Shuster
 
-import pandas as pd #xxlrd xlrd3 try install
-xl = pd.read_excel("P2M_prof.xlsx", dtype=str)
-
+import pandas as pd #xxlrd xlrd3 try to install if pandas isn't working
+xl = pd.read_excel("P2M012_G7.xlsx")
 # sets two global variables (bins and chars) to the values set in
 # ^the excel file
-bins = list(xl["bins"]) #sets binary numbers to list as bins
-chars = list(xl["chars"]) #sets characters to list as chars
+bins = list(xl["Bins"]) #sets binary numbers to list as bins
+chars = list(xl["Chars"]) #sets characters to list as chars
 
 compareDict = {} #empty dictionary: compares pairs from bins and chars lists
 
